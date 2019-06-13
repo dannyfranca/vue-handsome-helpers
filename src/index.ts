c9b8272e26e6f3a002fc5d4a9e13dcfaa6edb301
danny <danny_vcf@hotmail.com>
2019-06-13 17:13:41 -0300

    - Changes to pair with js-handsome-helpers original package

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,13 @@
-// @ts-ignore: Cannot find module
 import _Vue from 'vue'
-// @ts-ignore: Cannot find module
 import {PluginObject} from 'vue/types/plugin'
 // @ts-ignore: Cannot find module
-import typeOptions from '~/types/options'
+import typeOptions from './types/options'
 
-const helpers: PluginObject = {}
-
-helpers.install = function (Vue: typeof _Vue, options: typeOptions) {
-  const {scope = 'helpers'} = options
-  Vue.prototype['$' + scope] = require('@dannyfranca/handsome-helpers')
+const helpers: PluginObject<any> = {
+  install: function (Vue: typeof _Vue, options: typeOptions) {
+    const {scope = 'helpers'} = options
+    Vue.prototype['$' + scope] = require('@dannyfranca/handsome-helpers')
+  }
 }
 
 export default helpers
