b0a03dc4b73eea44b875de589aa646f8ac297f9a
danny <danny_vcf@hotmail.com>
2019-06-15 17:23:43 -0300

    - Added example that logs all functions to console

---
+++ example/App.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div id="app">
+    Open console and see the logs
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {}
+  },
+  mounted() {
+    console.log(this.$helpers)
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ example/index.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import Plugin from '../src/index.js'
+import App from './App.vue';
+
+Vue.use(Plugin)
+
+new Vue({
+  el: '#app',
+  render(h){ return h(App) }
+})
