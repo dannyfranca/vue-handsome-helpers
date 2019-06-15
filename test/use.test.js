28ec43e893dade094115c37bcdbb66321cc8772b
danny <danny_vcf@hotmail.com>
2019-06-15 17:24:36 -0300

    - Added render test in jest

--- test/use.test.js
+++ test/use.test.js
@@ -1,20 +1,29 @@
 import Vue from 'vue/dist/vue'
-import Helpers from '../src/index'
+import Plugin from '../src/index'
 
 let vm
 
 describe('basic', () => {
   // Install Plugin and mount instance
   beforeAll(async () => {
-    Vue.use(Helpers)
-    const template = `<div></div>`
+    Vue.use(Plugin)
+    const template = `<div>{{message}}</div>`
     vm = new Vue({
+      data: { message: null },
       template
     }).$mount()
   })
   
   afterAll(() => {
     vm.$destroy()
+  })
+  
+  test('render', async (done) => {
+    vm.message = 'Working'
+    Vue.nextTick(() => {
+      expect(vm.$el.innerHTML).toContain('Working')
+      done()
+    })
   })
   
   test('check install', async (done) => {
