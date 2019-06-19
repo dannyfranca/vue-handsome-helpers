dcd60e5fd2cd03e3e3cd9996b0b114d3bd8ae86f
danny <danny_vcf@hotmail.com>
2019-06-19 13:51:58 -0300

    - Supports js-handsome-helpers 0.4 with proxyed version as default

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
-import helpers from '@dannyfranca/handsome-helpers'
-
 const plugin = {}
 
 plugin.install = function (Vue, options = {}) {
-  const { scope = 'helpers' } = options
+  const { scope = 'helpers', proxy = true } = options
+  let helpers
+  
+  if (proxy) helpers = require('@dannyfranca/handsome-helpers/lib/proxyHelpers').default
+  else helpers = require('@dannyfranca/handsome-helpers').default
+  
   Vue.prototype['$' + scope] = helpers
 }
 
